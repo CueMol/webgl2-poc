6f14c94c676035d4284f4fd06cde12e4895de3a2
Your Name <you@example.com>
2020-07-26 16:18:28 +0900

    VAO impl

--- src/webgl_render.js
+++ src/webgl_render.js
@@ -5,7 +5,7 @@ console.log("glmat:", glmat)
 
 const VERTEX_SIZE = 3; // vec3
 const COLOR_SIZE  = 4; // vec4
-const VERTEX_NUMS = 6 * 1000 * 1000;
+const VERTEX_NUMS = 6 * 1000; // * 1000;
 const STRIDE = (VERTEX_SIZE + COLOR_SIZE) * Float32Array.BYTES_PER_ELEMENT;
 const POSITION_OFFSET = 0;
 const COLOR_OFFSET = VERTEX_SIZE * Float32Array.BYTES_PER_ELEMENT;
@@ -29,7 +29,7 @@ function randomUniform() {
     return Math.random() - 0.5;
 }
 
-const tri_size = 0.1;
+const tri_size = 10.0;
 const vertices_orig = [
     -tri_size, tri_size,  0.0,
     1.0, 0.0, 0.0, 1.0,
@@ -45,10 +45,144 @@ const vertices_orig = [
     0.0, 0.0, 1.0, 1.0,
 ];
 
+const move_scl = 0.1;
+
+function loadShader(gl) {
+    let vertexShaderSource = fs.readFileSync(path.resolve(__dirname, 'vertex_shader.glsl'));
+    
+    // console.log("rawdata", vertexShaderSource);
+    
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vertexShaderSource);
+    gl.compileShader(vertexShader);
+    
+    const vShaderCompileStatus = gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS);
+    if(!vShaderCompileStatus) {
+        const info = gl.getShaderInfoLog(vertexShader);
+        console.log(info);
+    }
+    
+    let fragmentShaderSource = fs.readFileSync(path.resolve(__dirname, 'fragment_shader.glsl'));
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fragmentShaderSource);
+    gl.compileShader(fragmentShader);
+    
+    const fShaderCompileStatus = gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS);
+    if(!fShaderCompileStatus) {
+        const info = gl.getShaderInfoLog(fragmentShader);
+        console.log(info);
+    }
+    
+    const program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    
+    const linkStatus = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if(!linkStatus) {
+        const info = gl.getProgramInfoLog(program);
+        console.log(info);
+    }
+    
+    return program;
+}
+
+class GLObject {
+    constructor(offset_x, offset_y, offset_z, move_scl, col_scl) {
+        this._offset_x = offset_x;
+        this._offset_y = offset_y;
+        this._offset_z = offset_z;
+        this._move_scl = move_scl
+        this._color_scl = col_scl
+    }
+
+    init(gl, val, cal) {
+        let program = loadShader(gl);
+        gl.useProgram(program);
+        this._program = program;
+
+        // Get attrib/unif loc from program object
+        this._vertexAttribLocation = val
+        this._colorAttribLocation  = cal
+
+        // Prepare vertex data
+        this._vertices = new Float32Array(VERTEX_NUMS * (VERTEX_SIZE + COLOR_SIZE));
+        for (let i=0; i<VERTEX_NUMS/6; ++i) {
+            const bias = i * 6 * (VERTEX_SIZE + COLOR_SIZE); 
+            for (let j=0; j<6*(VERTEX_SIZE + COLOR_SIZE); ++j) {
+                this._vertices[bias + j] = vertices_orig[j];
+            }
+        }
+        for (let i=0; i<VERTEX_NUMS; ++i) {
+            const bias = (VERTEX_SIZE + COLOR_SIZE) * i;
+            this._vertices[bias + 0] += this._offset_x;
+            this._vertices[bias + 1] += this._offset_y;
+            this._vertices[bias + 2] += this._offset_z;
+        }
+
+        // Prepare VAO
+        this._vao = gl.createVertexArray();
+        gl.bindVertexArray(this._vao);
+
+        gl.enableVertexAttribArray(this._vertexAttribLocation);
+        gl.enableVertexAttribArray(this._colorAttribLocation);
+
+        // Prepare VBO
+        this._vertexBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this._vertexBuffer);
+        gl.vertexAttribPointer(this._vertexAttribLocation, VERTEX_SIZE, gl.FLOAT, false, STRIDE, POSITION_OFFSET);
+        gl.vertexAttribPointer(this._colorAttribLocation, COLOR_SIZE, gl.FLOAT, false, STRIDE, COLOR_OFFSET);
+
+        // Transfer VBO to GPU
+        gl.bufferData(gl.ARRAY_BUFFER, this._vertices, gl.STATIC_DRAW);
+
+        // reset
+        gl.bindVertexArray(null);
+        gl.useProgram(null);
+        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+        
+        console.log("Initialized", VERTEX_NUMS * (VERTEX_SIZE + COLOR_SIZE));
+    }
+
+    changeData() {
+         for (let i=0; i<VERTEX_NUMS; ++i) {
+        //for (let i=0; i<6 * 10; ++i) {
+            const bias = (VERTEX_SIZE + COLOR_SIZE) * i;
+            this._vertices[bias + 0] += randomUniform() * this._move_scl;
+            this._vertices[bias + 1] += randomUniform() * this._move_scl;
+            this._vertices[bias + 2] += randomUniform() * this._move_scl;
+
+            this._vertices[bias + 3] = clamp(0, 1, randomUniform()*this._color_scl + this._vertices[bias + 3]);
+            this._vertices[bias + 4] = clamp(0, 1, randomUniform()*this._color_scl + this._vertices[bias + 4]);
+            this._vertices[bias + 5] = clamp(0, 1, randomUniform()*this._color_scl + this._vertices[bias + 5]);
+        }
+    }
+
+    draw(gl) {
+        this.changeData();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this._vertexBuffer);
+        gl.bufferSubData(gl.ARRAY_BUFFER, 0, this._vertices);
+        // gl.bufferData(gl.ARRAY_BUFFER, this._vertices, gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+        gl.bindVertexArray(this._vao);
+        gl.drawArrays(gl.TRIANGLES, 0, VERTEX_NUMS);
+
+        // reset
+        gl.bindVertexArray(null);
+    }
+}
+
 module.exports = class WebGLRender {
     constructor() {
         this._radian = 0;
         this._dist = 100;
+
+        this._data = [];
+        this._data[0] = new GLObject(0, 0, 0,  0.1, 0.001);
+        this._data[1] = new GLObject(0, 0, 50, 1.0, 0.2);
+        this._data[2] = new GLObject(0, 50, 0, 0.2, 0.1);
+        this._data[3] = new GLObject(50, 0, 0, 0.5, 0.5);
     }
 
     setUpModelMat(gl, radian) {
@@ -76,39 +210,16 @@ module.exports = class WebGLRender {
     }
 
 
-    setupProjectionUniforms(gl, radian) {
-        const model = glmat.mat4.create();
-        glmat.mat4.identity(model);
-
-        const fovY = 60 * Math.PI / 180;
-        const aspect = 500 / 500;
-        const near = 30;
-        const far  = 300;
-        const projection = glmat.mat4.create();
-        glmat.mat4.perspective(projection, fovY, aspect, near, far);
-
-        gl.uniformMatrix4fv(this._modelLocation, false, model);
-        gl.uniformMatrix4fv(this._projectionLocation, false, projection);
-
-        const cameraPosition = [Math.sin(radian)*radius, 100.0, Math.cos(radian)*radius];
-        const lookAtPosition = [0, 0, 0];
-        const upDirection    = [0.0, 1.0, 0.0];
-        const view  = glmat.mat4.create();
-        glmat.mat4.lookAt(view, cameraPosition, lookAtPosition, upDirection);
-        gl.uniformMatrix4fv(this._viewLocation, false, view);
-    }
-
-
-    init(canvas) {
+    init_old(canvas) {
         const gl = canvas.getContext('webgl2');
-
-        let program = this.loadShader(gl);
-        gl.useProgram(program);
-        this._program = program;
 
         gl.enable(gl.DEPTH_TEST);
         // gl.enable(gl.CULL_FACE);
         gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+        let program = loadShader(gl);
+        gl.useProgram(program);
+        this._program = program;
 
         // Get attrib/unif loc from program object
         this._vertexAttribLocation = gl.getAttribLocation(program, 'vertexPosition');
@@ -126,11 +237,16 @@ module.exports = class WebGLRender {
             }
         }
 
+        // Prepare VAO
+        this._vao = gl.createVertexArray();
+        gl.bindVertexArray(this._vao);
+
+        gl.enableVertexAttribArray(this._vertexAttribLocation);
+        gl.enableVertexAttribArray(this._colorAttribLocation);
+
         // Prepare VBO
         this._vertexBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, this._vertexBuffer);
-        gl.enableVertexAttribArray(this._vertexAttribLocation);
-        gl.enableVertexAttribArray(this._colorAttribLocation);
         gl.vertexAttribPointer(this._vertexAttribLocation, VERTEX_SIZE, gl.FLOAT, false, STRIDE, POSITION_OFFSET);
         gl.vertexAttribPointer(this._colorAttribLocation, COLOR_SIZE, gl.FLOAT, false, STRIDE, COLOR_OFFSET);
 
@@ -138,14 +254,41 @@ module.exports = class WebGLRender {
         gl.bufferData(gl.ARRAY_BUFFER, this._vertices, gl.STATIC_DRAW);
 
         // reset
+        gl.bindVertexArray(null);
         gl.useProgram(null);
         gl.bindBuffer(gl.ARRAY_BUFFER, null);
         
         console.log("Initialized", VERTEX_NUMS * (VERTEX_SIZE + COLOR_SIZE));
     }
 
+    init(canvas) {
+        const gl = canvas.getContext('webgl2');
+
+        gl.enable(gl.DEPTH_TEST);
+        // gl.enable(gl.CULL_FACE);
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+        let program = loadShader(gl);
+        gl.useProgram(program);
+        this._program = program;
+
+        // Get attrib/unif loc from program object
+        this._vertexAttribLocation = gl.getAttribLocation(program, 'vertexPosition');
+        this._colorAttribLocation  = gl.getAttribLocation(program, 'color');
+        this._modelLocation = gl.getUniformLocation(program, 'model');
+        this._viewLocation = gl.getUniformLocation(program, 'view');
+        this._projectionLocation = gl.getUniformLocation(program, 'projection');
+
+        this._data.forEach((value) => { value.init(gl, this._vertexAttribLocation, this._colorAttribLocation); });
+
+        // reset
+        gl.bindVertexArray(null);
+        gl.useProgram(null);
+        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    }
+
     changeData() {
-        const scl = 0.1;
+        const scl = 1.0;
     
          for (let i=0; i<VERTEX_NUMS; ++i) {
         //for (let i=0; i<6 * 10; ++i) {
@@ -163,7 +306,6 @@ module.exports = class WebGLRender {
 
     render(canvas) {
         const gl = canvas.getContext('webgl2');
-        // let program = this.loadShader(gl);
 
         this._radian += 1.0 * Math.PI / 180;
 
@@ -173,26 +315,16 @@ module.exports = class WebGLRender {
 
         gl.useProgram(this._program);
 
-        // this.setupProjectionUniforms(gl, this._radian);
         this.setUpModelMat(gl, this._radian);
         let cx = canvas.width;
         let cy = canvas.height;
         gl.viewport(0, 0, cx, cy);
         this.setUpProjMat(gl, cx, cy);
 
-        this.changeData();
-        gl.bindBuffer(gl.ARRAY_BUFFER, this._vertexBuffer);
-        gl.bufferSubData(gl.ARRAY_BUFFER, 0, this._vertices);
-        // gl.bufferData(gl.ARRAY_BUFFER, this._vertices, gl.STATIC_DRAW);
-
-        // gl.enableVertexAttribArray(this._vertexAttribLocation);
-        // gl.enableVertexAttribArray(this._colorAttribLocation);
-        // gl.vertexAttribPointer(this._vertexAttribLocation, VERTEX_SIZE, gl.FLOAT, false, STRIDE, POSITION_OFFSET);
-        // gl.vertexAttribPointer(this._colorAttribLocation, COLOR_SIZE, gl.FLOAT, false, STRIDE, COLOR_OFFSET);
-
-        gl.drawArrays(gl.TRIANGLES, 0, VERTEX_NUMS);
+        this._data.forEach((value) => { value.draw(gl); });
 
         // reset
+        gl.bindVertexArray(null);
         gl.useProgram(null);
         gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
@@ -200,43 +332,4 @@ module.exports = class WebGLRender {
         // console.log("render OK.", this._radian);
     }
 
-    loadShader(gl) {
-        let vertexShaderSource = fs.readFileSync(path.resolve(__dirname, 'vertex_shader.glsl'));
-
-        // console.log("rawdata", vertexShaderSource);
-        
-        const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-        gl.shaderSource(vertexShader, vertexShaderSource);
-        gl.compileShader(vertexShader);
-        
-        const vShaderCompileStatus = gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS);
-        if(!vShaderCompileStatus) {
-            const info = gl.getShaderInfoLog(vertexShader);
-            console.log(info);
-        }
-
-        let fragmentShaderSource = fs.readFileSync(path.resolve(__dirname, 'fragment_shader.glsl'));
-        const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-        gl.shaderSource(fragmentShader, fragmentShaderSource);
-        gl.compileShader(fragmentShader);
-
-        const fShaderCompileStatus = gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS);
-        if(!fShaderCompileStatus) {
-            const info = gl.getShaderInfoLog(fragmentShader);
-            console.log(info);
-        }
-
-        const program = gl.createProgram();
-        gl.attachShader(program, vertexShader);
-        gl.attachShader(program, fragmentShader);
-        gl.linkProgram(program);
-
-        const linkStatus = gl.getProgramParameter(program, gl.LINK_STATUS);
-        if(!linkStatus) {
-            const info = gl.getProgramInfoLog(program);
-            console.log(info);
-        }
-
-        return program;
-    }
 }
